54a57e07108b94715f5bb176e5dafb58c8ab1ecf
grey-codes65535 <steve@hartofchicago.com>
2026-06-21 05:40:51 -0400

    Added Launch Script and .desktop file.

---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Move into the project directory so paths resolve correctly
+cd ~/code/keyboard-fusion-rgb/GUI\ WIP/
+
+# Execute your main script using your system's python environment
+python3 gui_app.py
